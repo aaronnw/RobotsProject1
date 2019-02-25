0d77d8e674cba478db2d9e41b8d0b8b4f1d24af9
Aaron Wilson <aaronnw11@gmail.com>
2019-02-25 14:09:26 -0600

    Added a project catkin workspace with a package and some basic launch files to use the world models

---
+++ project_workspace/build/proj1_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/RobotsProject1/project_workspace/src/proj1_pkg
+# Build directory: /home/student/RobotsProject1/project_workspace/build/proj1_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
