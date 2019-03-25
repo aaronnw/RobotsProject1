d3c2a058f98a32a695cc9aaaa65a53907e50ef33
Aaron Wilson <aaronnw11@gmail.com>
2019-03-25 13:50:53 -0500

    Everything is working. maybe

--- project_workspace/src/proj1_pkg/scripts/control_robot.py
+++ project_workspace/src/proj1_pkg/scripts/control_robot.py
@@ -45,10 +45,10 @@ def turn(angle, duration):
 	for i in range(duration):
 		if _key_input_lock:
 		   break
-		print('actively turning')
+		#print('actively turning')
 		_cmd_vel.publish(turn_cmd)
 		r.sleep()
-	print('done turning')
+	#print('done turning')
 	_cmd_vel.publish(Twist())
 	r.sleep()
 
@@ -64,7 +64,7 @@ def go_forward():
 	global _hz
 	global _cmd_vel
 	distance = 0  # distance traveled since last random turn, or since last lock release
-	max_straight_distance = 18  # maximum distance to travel before making a random turn
+	max_straight_distance = 23  # maximum distance to travel before making a random turn
 	key_input_lock_delay = 3  # number of seconds to pause after detecting a key press
 
 	# keep the thread running until the program receives the shutdown signal
@@ -95,7 +95,7 @@ def go_forward():
 			# track the distance traveled
 			distance += 1
 			# if we've gone about a foot, then initiate the random turn protocol
-			#_turning_lock = distance >= max_straight_distance
+			_turning_lock = distance >= max_straight_distance
 
 		# turn randomly (uniformly sampled within +/- 15 degrees) after every 1ft of forward movement.
 		elif _turning_lock:
@@ -103,8 +103,9 @@ def go_forward():
 			distance = 0  # reset the distance counter
 			_turning_lock = True
 			direction = random.choice([-1, 1]) 
-			rng = random.randint(1, 12)
-			turn(30*direction, rng)
+			rng = random.randint(1, 8)
+			turn_speed = 35
+			turn(turn_speed*direction, rng)
 			_turning_lock = False
 
 def escape():
@@ -114,18 +115,17 @@ def escape():
 	global _left_avg
 	global _right_avg
 
-	turn_time = 25
+	turn_time = 7
 	# Speed in rad/s for all turns
-	turn_speed = 30
-	while True:
+	turn_speed = 35
+	while not _shutdown_flag:
 		if _key_input_lock or _turning_lock:
-			print("LOCKED BY KEYS")
-			return
-		elif _left_avg < 850 and _right_avg < 850:
+			continue
+		elif _left_avg < 885 and _right_avg < 885:
 			print(_left_avg, _right_avg)
 			print("\nEscaping symmetrical obstacles")
 			_escape_lock = True
-			turn(turn_speed, 35)	 # turn around ~180 degrees
+			turn(turn_speed, 33)	 # turn around ~180 degrees
 			time.sleep(1)	 # give it a little time to finish the turn
 			_escape_lock = False
 		# if the left side is too close, turn right
